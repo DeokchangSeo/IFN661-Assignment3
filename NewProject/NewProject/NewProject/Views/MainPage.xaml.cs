da29cee03f6e4076cf24a1bd649afdb0eb7c1360
CHRIS SEO <deokchang.seo@qut.edu.au>
2016-05-18 15:44:42 +1000

    Added base project
    Added base project made by Sneha

---
+++ NewProject/NewProject/NewProject/Views/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace NewProject.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+       public void OnButtonClick1(Object o, EventArgs e)
+        {
+            Navigation.PushAsync(new Page1());
+        }
+        public void OnButtonClick2(Object o, EventArgs e)
+        {
+            Navigation.PushAsync(new Page2());
+        }
+        public void OnButtonClick3(Object o, EventArgs e)
+        {
+            Navigation.PushAsync(new Page3());
+        }
+    }
+}
